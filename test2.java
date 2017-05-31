711e34a30d1355dbcbaa63cd415c30b705d1fb26
Waynelee94 <zhangruijian1994@126.com>
2017-05-31 15:10:47 +0800

    101-200素数问题
    判断101-200之间有多少个素数，并输出所有素数。 
    
    素数又叫质数，就是除了1和它本身之外，再也没有整数能被它整除的数。也就是素数只有两个因子。

---
+++ test2.java
@@ -0,0 +1,20 @@
+package test1;
+import java.util.*;
+import java.math.*;
+
+public class test2 {	
+	public static void main(String[] args) {  
+		 int i,j,n,m,x;      //n是用来存储余数的；m是用来统计具体一个数的因子；  
+		    n=0;m=0;x=0;        //x是用来统计101~200之前素数的个数  
+		    for(i=101;i<=200;i++){             //两重循环  
+		        for(j=1;j<=i;j++){         
+		            n=i%j;                    //去余数，如果余数为零，就是该数的因子  
+		            if(n==0)  { m=m+1; }      //统计某数有多少个因子  
+		            }   
+		     if(m==2){System.out.print(i+" "); x=x+1;} //如果某数的因子只有两个，那它就一定是素数，那这个数就应该输出  
+		          m=0;            //一定要清零，不然会继续累加  
+		        }  
+		       System.out.println();  
+		       System.out.println("在101~200之間一共有素數："+x+"個");  
+		        }  
+		   }  
