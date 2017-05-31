d2d95419670f24d89155eac1e7df7c5eafc000a5
Waynelee94 <zhangruijian1994@126.com>
2017-05-31 15:07:10 +0800

    水仙花数问题
    判断101-200之间有多少个素数，并输出所有素数。 
    
    素数又叫质数，就是除了1和它本身之外，再也没有整数能被它整除的数。也就是素数只有两个因子。

--- test1.java
+++ test1.java
@@ -4,21 +4,13 @@ import java.math.*;
 
 public class test1 {	
 	public static void main(String[] args) {
-		int count = 0;//记录因子的个数
-		int sushu = 0;//记录素数的个数
-		int yushu = 0;//记录余数
-		int i, j;//i是101-200之间的数，j是除数
-		for (i = 101; i <=200; i++) {
-			for (j = 1; j <= i; j++) {
-				yushu = i % j;
-				if (yushu == 0) {count += 1;}
-				}
-			if (count == 2) {
-				System.out.print(i + " , ");
-				sushu += 1;
-				}
-			count = 0;
+		int ge, shi, bai, sum;//个位十位百位；sum是各位立方之和
+		for (int number = 100; number <= 999; number++) {
+			bai = number / 100;
+			shi = (number - bai * 100) / 10 ;
+			ge = number - (bai * 100) - (shi * 10);
+			sum = (int) Math.pow(bai, 3) + (int) Math.pow(shi, 3) + (int) Math.pow(ge, 3);
+			if (sum == number) {System.out.print(number + " ");}
 		}
-		System.out.println("共有素数" + sushu + "个。");
 	}
 }
